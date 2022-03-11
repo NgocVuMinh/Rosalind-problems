1c6819d2198b4d05f56ca74d912802d94ab07044
Ngoc Vu <101399789+NgocVuMinh@users.noreply.github.com>
2022-03-11 20:31:01 +0700

    Update and rename RNAsplicing.py to 16-RNAsplicing.py

--- 16-RNAsplicing.py
+++ 16-RNAsplicing.py
@@ -1,3 +1,15 @@
+#     See more (including information on discontiguous genes and sample datasets) at https://rosalind.info/problems/splc/
+
+#     PROBLEM:
+#     After identifying the exons and introns of an RNA string, we only need to delete the introns and concatenate the exons to form a new string ready for translation.
+#     Given: A DNA string s (of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are given in FASTA format.
+#     Return: A protein string resulting from transcribing and translating the exons of s. (Note: Only one solution will exist for the dataset provided.)
+
+#     !!! NOTICE that the actual dataset that Rosalind gives you is in FASTA format so it looks something more like this (need to take into account the seperated lines):
+#     >Rosalind_1
+#     ATCCAGCT
+#     GGGCAACT
+#______________________________ MY SOLUTION _________________________________
 
 
 codon = """
