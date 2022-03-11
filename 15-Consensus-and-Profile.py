8136429ae334c6e3b820f76b7c03e39ef2d134a6
Ngoc Vu <101399789+NgocVuMinh@users.noreply.github.com>
2022-03-11 20:38:11 +0700

    Update and rename Consensus.py to 15-Consensus-and-Profile.py

--- 15-Consensus-and-Profile.py
+++ 15-Consensus-and-Profile.py
@@ -1,9 +1,3 @@
-print("""
-
-
-
-""")
-
 #     See more (including information on consensus and profile and sample datasets) at https://rosalind.info/problems/cons/
 
 #     PROBLEM:
@@ -16,64 +10,43 @@ print("""
 #     Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
 #     Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
 
-#     !!! NOTICE that the actual dataset that Rosalind gives you looks something more like this (so you have to take into account the seperated lines):
+#     !!! NOTICE that the actual dataset that Rosalind gives you is in FASTA format, so it looks something more like this (need to take into account the seperated lines):
 #     >Rosalind_1
 #     ATCCAGCT
 #     GGGCAACT
-#___________________________________________________________________________________________________________________________________________
+#____________________________________ MY SOLUTION _______________________________________
 
-#     MY SOLUTION:
 
 import pandas as pd
 import re
-pd.set_option("display.max_columns", None)
+pd.set_option("display.max_columns", None)                 # displaying all columns since the output dataframe is huge (>900 columns)
 
-#----------------Dealing with the file-----------------------
-download = open('rosalind_cons5.txt','r').read()
-trimread = download.replace('\n','')
-split = re.split('>',trimread)
-split = split[1:]
+fas = open('rosalind_cons5.txt','r').read()                # dealing with the fasta format
+trim = fas.replace('\n','')
+split = re.split('>',trim)[:1]
 file = []
 for strand in split:
     strand = strand[13:]
     file.append(strand)
-#------------------------------------------------------------
 
-df = pd.DataFrame()
-
+df = pd.DataFrame()                                        # creating a blank dataframe, then append DNA strings in as rows
 def split_dna(dna):
     return [base for base in dna]
-
 for i in range(len(file)):
     df[i+1] = split_dna(file[i])
-    
 df1 = df.transpose()
 
-profile = pd.DataFrame({'base': ["A", "C", "G", "T"], 'e': [0, 0, 0, 0]})
+profile = pd.DataFrame({'base': ["A", "C", "G", "T"], 'e': [0, 0, 0, 0]})                # creating the profile
 profile.set_index('base', inplace=True)
-
 for k in df1.columns:
     profile['P'+str(k)] = df1[k].value_counts()
-
 profile.drop('e', axis=1, inplace=True)
 profile.fillna(0, inplace=True)
 
-con_list = []
+con_list = []                                                        # creating the consensus
 for col in profile:
     con_list.append(profile[col].idxmax())
 con = ''.join(con_list)
 
 print(con)
 print(profile)
-
-
-
-
-
-
-
-print("""
-
-
-
-""")
